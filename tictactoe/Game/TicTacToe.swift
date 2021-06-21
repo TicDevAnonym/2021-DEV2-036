2b6b42bece0e5aec8addfb6f42b937d75c780b94
ibrahim oktay <ibrahimoktay@yahoo.com>
2021-06-21 11:01:51 +0300

    Add game state for invalid movement & win

--- tictactoe/Game/TicTacToe.swift
+++ tictactoe/Game/TicTacToe.swift
@@ -13,11 +13,32 @@ class TicTacToe {
     
     var board = Array(repeating: TicTacToe.empty, count: 9)
     var activePlayer: Player = Player.X
-    
-    func makeMove(at index: Int) {
+    var state = GameState.playing
+
+    func makeMove(at index: Int) -> GameState {
+        // Invalid move
+        if board[index] != TicTacToe.empty {
+            return .invalidMove
+        }
         // Move
         board[index] = activePlayer.sign
+        
+        // Win
+        for winningBoard in TicTacToe.winningBoards {
+            if board[winningBoard[0]] != TicTacToe.empty && board[winningBoard[0]] == board[winningBoard[1]] && board[winningBoard[0]] == board[winningBoard[2]] {
+                if board[winningBoard[0]] == Player.X.sign {
+                    print("X wins")
+                    return .win(Player.X)
+                } else {
+                    print("O wins")
+                    return .win(Player.O)
+                }
+            }
+        }
+        
+     
         activePlayer = activePlayer == Player.X ? Player.O : Player.X
+        return .playing
     }
     
     func reset() {
